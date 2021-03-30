2baf71debbb10afde81873537e2cd2a36cd119bf
ShivaKumarM999 <manchikatlashivakumar999@gmail.com>
2021-03-30 14:06:06 +0530

    Commiting DB first Project

---
+++ EFDF/SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].Department
+(
+	[DeptId] INT NOT NULL PRIMARY KEY, 
+    [DeptName] VARCHAR(50) NULL, 
+    [Location] VARCHAR(50) NULL
+)
+
+CREATE TABLE [dbo].Employee
+(
+	[EId] INT NOT NULL PRIMARY KEY, 
+    [EName] VARCHAR(50) NULL, 
+    [Salary] MONEY NULL, 
+    [DeptId] INT NULL FOREIGN KEY REFERENCES Department(DeptId)
+)
